44350d7227de61b8bbb5a6c08ed5164c0bd1e757
Cojocaru Cosmin <alexandru.cojoc1@gmail.com>
2020-06-18 11:38:39 +0300

    aa

---
+++ Repository/src/main/java/scs/mpp/exam/repository/OJJ.java
@@ -0,0 +1,13 @@
+package scs.mpp.exam.repository;
+
+public class OJJ {
+    private String a;
+
+    public OJJ(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+}
